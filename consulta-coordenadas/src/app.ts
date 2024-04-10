97ae164432f7a76fac0f94364f97bbf45af8e1b8
Izabelle Cesario <izabellecesario@hotmail.com>
2024-04-10 17:29:28 -0300

    feat(p1): obtem coords a partir de cidade

---
+++ consulta-coordenadas/src/app.ts
@@ -0,0 +1,52 @@
+import express from 'express'
+
+require('dotenv').config()
+
+
+const axios = require ('axios')
+
+
+
+//const url = `${URL_BASE}?q=${Q}&limit=${LIMIT}&appid=${APPID}&lang=${LANGUAGE}`
+//console.log(url)
+const app = express()
+app.use(express.json())
+
+interface LatLon {
+    id: string;
+    cidade: string;
+    lat: number;
+    lon: number;
+}
+
+const coordenadas: Record<string, LatLon> = {}
+let id: string = '1'
+
+app.get('/consulta_coordenadas', (req, res) => {
+    res.json(coordenadas)
+})
+
+app.post('/consulta_coordenadas', async (req, res) => {
+    const  { cidade }  = req.body
+    const { APPID, LIMIT, LANGUAGE,  URL_BASE } = process.env
+    const url = `${URL_BASE}?q=${cidade}&limit=${LIMIT}&appid=${APPID}&lang=${LANGUAGE}`
+    
+    try{
+        const response = await axios.get(url)
+        const {lat, lon } = response.data[0]
+        console.log(lat, lon)
+        const LatLon = { id, cidade, lat, lon }
+        coordenadas[id] = LatLon
+        id = (+id + 1).toString()
+        res.json(LatLon)
+    }
+    catch (erro){
+        res.status(400).json({ erro: 'Cidade não encontrada.'})
+    
+    }
+
+})
+
+
+const port = 4000
+app.listen(port,() => console.log(`Coordenadas. Porta ${port}.`))
